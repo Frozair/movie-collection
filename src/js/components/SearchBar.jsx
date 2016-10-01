c99eafcfc7f816b7af7c93646817e0cbeb1cb569
Rudolph Newball <newrdev@gmail.com>
2016-10-01 11:34:33 -0500

    Searchbar component

---
+++ src/js/components/SearchBar.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { FormGroup, ControlLabel, FormControl, Button } from "react-bootstrap";
+
+export default class SearchBar extends React.Component
+{
+  handleChange(event) {
+    this.props.onUserInput(event.target.value);
+  }
+
+  render() {
+    return (
+      <form>
+        <FormGroup>
+          <ControlLabel>Search through your collection</ControlLabel>
+          <FormControl
+            type="text"
+            placeholder="The Matrix, Keanu Reeves, 1999, etc"
+            onChange={this.handleChange.bind(this)}
+          />
+        </FormGroup>
+      </form>
+    );
+  }
+};
