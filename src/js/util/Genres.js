f6fd5f69ae9979f6e02cd9af4359cef5aedbb1cb
Rudolph Newball <newrdev@gmail.com>
2016-10-01 18:49:47 -0500

    List of possible genres

---
+++ src/js/util/Genres.js
@@ -0,0 +1,23 @@
+const Genres = [
+  "Action",
+  "Adventure",
+  "Adult",
+  "Animation",
+  "Comedy",
+  "Crime",
+  "Documentary",
+  "Drama",
+  "Fantasy",
+  "Film-Noir",
+  "Horror",
+  "Musical",
+  "Mystery",
+  "Romance",
+  "Sci-Fi",
+  "Short",
+  "Thriller",
+  "War",
+  "Western",
+];
+
+export default Genres;
