c5d468e13762d5318306c5189e666745123cae17
Rudolph Newball <newrdev@gmail.com>
2016-10-01 23:49:52 -0500

    Movie listing is now prettier

--- src/js/components/MovieRow.jsx
+++ src/js/components/MovieRow.jsx
@@ -1,12 +1,27 @@
 import React from "react";
 import { Panel } from "react-bootstrap";
+import StarRatingComponent from 'react-star-rating-component';
 
 export default class MovieRow extends React.Component
 {
   render() {
+    const rating = parseInt(this.props.movie.rating);
+
     return (
       <Panel>
-        {this.props.movie.title}
+        <h4>{this.props.movie.title}</h4>
+
+        <p><strong>Genre: </strong>{this.props.movie.genre}</p>
+        <p><strong>Actors: </strong>{this.props.movie.actors}</p>
+
+        <StarRatingComponent
+          id="rating"
+          className="large"
+          name="starRating"
+          starCount={5}
+          value={rating}
+          editing={false}
+        />
       </Panel>
     );
   }
