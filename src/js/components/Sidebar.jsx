553fb2559cfba45af9a20fa14ec69d6a18cff8b0
Rudolph Newball <newrdev@gmail.com>
2016-10-01 23:28:43 -0500

    Add tooltip to side bar menu

--- src/js/components/Sidebar.jsx
+++ src/js/components/Sidebar.jsx
@@ -1,16 +1,22 @@
 import React from "react";
 import { browserHistory } from 'react-router';
 import style from "../../styles/sidebar.scss";
-import { Glyphicon, Nav, NavItem } from "react-bootstrap";
+import { Glyphicon, Nav, NavItem, OverlayTrigger, Tooltip } from "react-bootstrap";
 
 export default class Sidebar extends React.Component
 {
   render() {
+    const homeTooltip = (<Tooltip id="homeTooltip">Home</Tooltip>);
+    const addTooltip = (<Tooltip id="addTooltip">Add New Movie</Tooltip>);
     return (
       <div className="side-bar">
         <Nav stacked activeKey={1} >
-          <NavItem eventKey={1} title="Home" href="/#"><Glyphicon className="large" glyph="home" /></NavItem>
-          <NavItem eventKey={2} title="New Movie" href="/#new"><Glyphicon className="large" glyph="plus" /></NavItem>
+          <OverlayTrigger placement="right" overlay={homeTooltip}>
+            <NavItem eventKey={1} title="Home" href="/#"><Glyphicon className="large" glyph="home" /></NavItem>
+          </OverlayTrigger>
+          <OverlayTrigger placement="right" overlay={addTooltip}>
+            <NavItem eventKey={2} title="New Movie" href="/#new"><Glyphicon className="large" glyph="plus" /></NavItem>
+          </OverlayTrigger>
         </Nav>
       </div>
     );
